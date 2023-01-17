cc14681e2b33ae13cc6673a1ac880798cc545457
kim kyuinghoon <kkh9172@naver.com>
2023-01-17 11:28:30 +0900

    Initial Commit

---
+++ WatchAppNew Watch App/WatchAppNewApp.swift
@@ -0,0 +1,17 @@
+//
+//  WatchAppNewApp.swift
+//  WatchAppNew Watch App
+//
+//  Created by Salted on 2023/01/17.
+//
+
+import SwiftUI
+
+@main
+struct WatchAppNew_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
